3f73d8c44ad22572ebbdccd8d08d0080f0904a16
jahkid <jahkidedeh@gmail.com>
2022-08-20 16:09:53 +0100

    users app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:myride/authentication/login_screen.dart';
+// import 'package:myride/authentication/login_screen.dart';
+import 'package:myride/mainScreens/mainScreen.dart';
 // import 'package:myride/mainScreens/mainScreen.dart';
 // import 'package:myride/authentication/signup_screen.dart';
 
@@ -28,65 +29,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
-      home: LoginScreen(),
+      home: homeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-   
-//     return Scaffold(
-//       appBar: AppBar(
-        
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-        
-//         child: Column(
-          
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), 
-//     );
-//   }
-// }
